77a98edfca4d6d15d5c51d910c91e019239542fc
Lisa <e.shmakova@itsrizzoli.it>
2019-03-19 11:02:18 +0100

    Aggiunto tutorial iniziale di Andrea

--- app/src/main/java/com/petit/LaunchActivity.java
+++ app/src/main/java/com/petit/LaunchActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.petit.tutorial.TutorialActivity;
+
 public class LaunchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, MainActivity.class));
+        startActivity(new Intent(this, TutorialActivity.class));
         finish();
     }
 }
